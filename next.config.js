f0b0ed654f69d93bdeab9a587120d75c3ef1f3ca
Andre Nunes <andrenunes.work@gmail.com>
2024-02-12 00:59:47 +0000

    Update next config

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
         hostname: "127.0.0.1",
         port: "54321",
       },
+      {
+        protocol: "https",
+        hostname: "**.supabase.co/",
+        port: "",
+      },
     ],
   },
 }
