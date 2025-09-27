be523a4d458ded22691215782d1dcf4de0fe403b
Andre Nunes <andrenunes.work@gmail.com>
2025-09-27 15:27:47 +0100

    Refactor sponsors page to include clickable links for partner logos, enhancing user engagement with external sponsors' Instagram profiles.

--- app/[locale]/sponsors/page.tsx
+++ app/[locale]/sponsors/page.tsx
@@ -1,6 +1,7 @@
 import { PageLayout } from '@/components/layout/Page'
 import { t } from '@/lib/i18n'
 import Image from 'next/image'
+import Link from 'next/link'
 
 interface SponsorsPageProps {
   params: Promise<{
@@ -16,20 +17,77 @@ const Page = async ({ params }: SponsorsPageProps) => {
     <PageLayout headerTitle="sponsors" locale={locale}>
       <div className="w-2/3 md:w-1/2 mx-auto py-24 max-w-md">
         <div className="flex flex-col gap-20">
-          <Image alt="fran-logo" src="/assets/cork.png" fill className="!relative" />
+          <Link
+            href="https://www.instagram.com/corkpadel/?hl=en"
+            target="_blank"
+            prefetch={false}
+            className="hover:shadow-lg hover:scale-105 transition-all duration-300 hover:bg-white pb-4"
+          >
+            <Image alt="fran-logo" src="/assets/cork.png" fill className="!relative" />
+          </Link>
 
-          <Image alt="fran-logo" src="/assets/DFL.png" fill className="!relative" />
+          <Link
+            href="https://www.instagram.com/dfl.selectedmeat/?hl=en"
+            target="_blank"
+            prefetch={false}
+            className="hover:shadow-lg hover:scale-105 transition-all duration-300"
+          >
+            <Image alt="fran-logo" src="/assets/DFL.png" fill className="!relative" />
+          </Link>
 
-          <Image alt="fran-logo" src="/assets/proframe.png" fill className="!relative" />
+          <Link
+            href="https://www.instagram.com/pro.frame_/?hl=en"
+            target="_blank"
+            prefetch={false}
+            className="hover:shadow-lg hover:scale-105 transition-all duration-300 hover:bg-white "
+          >
+            <Image alt="fran-logo" src="/assets/proframe.png" fill className="!relative" />
+          </Link>
 
-          <Image alt="an-logo" src="/assets/an-logo-color.svg" fill className="!relative my-[-92px] " />
+          <Link
+            href="https://www.instagram.com/anunes9/?hl=en"
+            target="_blank"
+            prefetch={false}
+            className="hover:shadow-lg hover:scale-105 transition-all duration-300 hover:bg-white"
+          >
+            <Image
+              alt="an-logo"
+              src="/assets/an-logo-color.svg"
+              fill
+              className="!relative my-[-72px] !w-60 mx-auto py-8"
+            />
+          </Link>
 
-          <h1 className="font-projectFontExtendedBlack text-lg sm:text-xl lg:text-[48px] underline underline-offset-8 decoration-4 text-center text-projectBlue mt-8">
+          <h2 className="font-projectFontExtendedBlack text-lg sm:text-xl lg:text-[48px] underline underline-offset-8 decoration-4 text-center text-projectBlue mt-8">
             {partnerClubText}
-          </h1>
+          </h2>
 
-          <Image alt="fran-logo" src="/assets/azores-banner.png" fill className="!relative mt-8" />
-          <Image alt="fran-logo" src="/assets/mcp-banner.png" fill className="!relative mt-8" />
+          <Link
+            href="https://www.instagram.com/azorespadelclub/?hl=en"
+            target="_blank"
+            prefetch={false}
+            className="hover:shadow-lg hover:scale-105 transition-all duration-300"
+          >
+            <Image alt="fran-logo" src="/assets/azores-banner.png" fill className="!relative mt-8" />
+          </Link>
+
+          <Link
+            href="https://www.instagram.com/mypadelcentermafra/?hl=en"
+            target="_blank"
+            prefetch={false}
+            className="hover:shadow-lg hover:scale-105 transition-all duration-300"
+          >
+            <Image alt="fran-logo" src="/assets/mcp-banner.png" fill className="!relative mt-8" />
+          </Link>
+
+          <Link
+            href="https://www.instagram.com/padelclub.premier/?hl=en"
+            target="_blank"
+            prefetch={false}
+            className="hover:shadow-lg hover:scale-105 transition-all duration-300"
+          >
+            <Image alt="fran-logo" src="/assets/premier-padel-banner.png" fill className="!relative mt-8" />
+          </Link>
         </div>
       </div>
     </PageLayout>
